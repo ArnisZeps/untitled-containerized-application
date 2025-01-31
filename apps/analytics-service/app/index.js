93923debc07544dc792bc8649c2eaa335ff2ddd4
Arnis Zeps <zeps.arnis@gmail.com>
2025-01-31 16:08:41 +0200

    feat: analytics service added

---
+++ apps/analytics-service/app/index.js
@@ -0,0 +1,19 @@
+import 'dotenv/config'
+import routes from "./routes/index.js"
+import cors from "cors";
+import express from "express";
+
+const app = express();
+const { PORT } = process.env;
+app.listen(PORT);
+app.use(
+  cors({
+    origin: ["http://localhost:3000"], 
+  }),
+);
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
+
+console.log("Listening on port", PORT);
+
---
+++ apps/analytics-service/app/routes/index.js
@@ -0,0 +1,20 @@
+import "dotenv/config";
+import express from "express";
+
+const router = express.Router();
+
+router.post("/analytics/page-view", async (req, res) => {
+  try {
+    res.status(200).send({
+      message: "added",
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({
+      info: "Request failed",
+      trace: err.message,
+    });
+  }
+});
+
+export default router;
